de0d63a3cbbeae82df11e994a7a927189123cef9
Agustin Avellaneda <agustin.avellaneda20@gmail.com>
2024-05-19 15:30:26 -0300

    Desafio semanal 8 finalizado

--- DaVinci ASB1 [Carpeta Raiz]/1° Cuatrimestre/Introduccion a la Programacion/Ejercicios/Todos los ejercicios (separados por archivo)/primer_cuatrimestre_ejercicios/src/S8_ds.java
+++ DaVinci ASB1 [Carpeta Raiz]/1° Cuatrimestre/Introduccion a la Programacion/Ejercicios/Todos los ejercicios (separados por archivo)/primer_cuatrimestre_ejercicios/src/S8_ds.java
@@ -62,7 +62,91 @@ public class S8_ds {
                 System.out.println("Numeros entre 75 y 100 : " + acumulador100);
                 break;
             case 2:
-                // code block
+                int desocupados = 0;
+                int primerRango = 0;
+                int segundoRango = 0;
+                int tercerRango = 0;
+                int cuartoRango = 0;
+
+                double sueldoMaximo = -999999.0;
+                String nombreSueldoMaximo = null;
+
+                int totalSueldosPagados = 0;
+
+                // Obtener las n personas
+                System.out.println("Ingrese la cantidad de personas: ");
+                int cantidadPersonas = sc.nextInt();
+
+                sc.next();
+
+                for (int i = 0; i < cantidadPersonas; i++) {
+                    System.out.println("Ingrese el nombre de la persona " + (i + 1) + ": ");
+                    String nombre = sc.next();
+
+                    System.out.println("Ingrese el sueldo de la persona " + (i + 1) + ": ");
+                    Double sueldo = sc.nextDouble();
+
+                    if (sueldo == 0) {
+                        desocupados++;
+                    } else if (sueldo < 500) {
+                        primerRango++; // hasta 500
+                    } else if (sueldo < 1000) {
+                        segundoRango++; // entre 500 y 1000
+                    } else if (sueldo <= 2000) {
+                        tercerRango++; // entre 1000 y 2000
+                    } else {
+                        cuartoRango++; // mayor a 2000
+                    }
+
+                    // El sueldo máximo y el nombre de la persona de sueldo máximo
+                    if (sueldo > sueldoMaximo && sueldo > 0) {
+                        sueldoMaximo = sueldo;
+                        nombreSueldoMaximo = nombre;
+                    }
+
+                    // • Total de sueldos pagados - OPCION 1
+                    if (sueldo > 0) {
+                        totalSueldosPagados++;
+                    }
+                }
+                break;
+            case 3:
+                // variables ejercicio 3
+                int minimo = 0, maximo = 0;
+                int num = 0;
+
+                System.out.println("Cuantos numeros seran ingresados? ");
+                while (!sc.hasNextInt()) { // validacion es por si el usuario NO ingresa un INT
+                    System.out.println("Por favor, ingrese un número valido.");
+                    sc.next(); // limpiar
+                }
+
+                int numerosIngresados = sc.nextInt();
+
+                for (int i = 0; i < numerosIngresados; i++) {
+
+                    System.out.print("Ingrese un numero: ");
+                    while (!sc.hasNextInt()) { // validacion es por si el usuario NO ingresa un INT
+                        System.out.println("Por favor, ingrese un número valido.");
+                        sc.next(); // limpiar
+                    }
+                    num = sc.nextInt();
+                    if (i == 0) {
+                        minimo = num;
+                        maximo = num;
+                    } else {
+                        if (num < minimo) {
+                            minimo = num;
+                        }
+                        if (maximo < num) {
+                            maximo = num;
+                        }
+                    }
+
+                }
+                int rango = maximo - minimo;
+                System.out.println("El minimo es: " + minimo + " y el maximo es: " + maximo);
+                System.out.println("El rango es: " + rango);
                 break;
             default:
                 // code block
